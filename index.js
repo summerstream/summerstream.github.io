6dc8d959605178775c2acbb8499dd014ce7ae061
culnoty <culnoty@outlook.com>
2017-10-08 20:19:23 +0800

    u

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var pic1 = document.getElementById('pic1');
-function scroll(e){
-    console.info(window.scrollY)
-    window.requestAnimationFrame(function(){
-        if(1){
-            var top = 200-window.scrollY/10+'px';
-            // top=100;
-            console.info(top)
-            pic1.style.top=top;
-        }else{
-
-        }
-    })
+var pic1 = document.getElementById("pic1");
+function scroll(e) {
+  console.info(window.scrollY);
+  window.requestAnimationFrame(function() {
+    if (1) {
+      var top = 200 - window.scrollY / 10;
+      if (top < 0) {
+        top = 0;
+      }
+      pic1.style.top = top + "px";
+    } else {
+    }
+  });
 }
-window.addEventListener('scroll', scroll)
+window.addEventListener("scroll", scroll);
