ff5f615abd31330872980fcd0c55bc25b4971553
culnoty <culnoty@outlook.com>
2017-10-15 18:14:26 +0800

    fall anim

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function fall(){
+  var fallTarget = document.getElementById('aboutCard');
+  var begin = 0;
+  var dura = 100;
+  var initTop = fallTarget.offsetTop
+  var height = fallTarget.clientHeight;
+  function bounce(){
+    begin++;
+    if(begin < dura){
+      var topValue = 0 - height + window.Tween.Bounce.easeOut(begin,0,initTop+height,dura);
+      fallTarget.style.top = topValue+'px';
+      window.requestAnimationFrame(bounce);
+    }
+  }
+  bounce();
+}
+window.requestAnimationFrame(fall);
+
 //bind scroll
 var pic1 = document.getElementById("pic1");
 function scroll(e) {
@@ -27,4 +45,4 @@ setTimeout(function(){
             })(i), i*100);
 
     }
-}, 300)
+}, 100*1000/60)
